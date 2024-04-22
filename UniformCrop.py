b3985e6508ce07001fd16c14218eaaf501b3e994
CamHickling <69331833+CamHickling@users.noreply.github.com>
2024-04-22 14:22:45 -0700

    Add files via upload

--- UniformCrop.py
+++ UniformCrop.py
@@ -5,10 +5,7 @@ from PIL import Image
 import os
 
 # given an image, find the bounding box of the non-transparent part of the image for an left page
-def find_non_white_bounding_boxL(image_path, min_width=10, min_height=10):
-    # change these values to match the desired width and height of the image for left pages
-    totalWidth = 2083 
-    totalHeight = 3415
+def find_non_white_bounding_boxL(image_path, totalWidth, totalHeight, min_width=10, min_height=10):
 
     #First, get or create an image number
     #in this case, the image number is the 4th element of the split string
@@ -68,7 +65,7 @@ def find_non_white_bounding_boxL(image_path, min_width=10, min_height=10):
     return bounding_boxes[0]
 
 # process all left page image in the input folder and save the resized images in the output folder
-def process_imagesL(input_folder, output_folder, target_size):
+def process_imagesL(input_folder, output_folder, width, height):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -79,28 +76,25 @@ def process_imagesL(input_folder, output_folder, target_size):
             continue  # Skip macOS metadata files
 
         # Only process left page images
-        # The image is a left page if it ends with "_l.png" or "_l.png"
+        # The image is a left page if it ends with "_l.png"
         # change these string in the ends with function to match the naming convention of all your left page images
         if filename.lower().endswith(("_l.png", "_l.png")):
             print(filename)
 
             # Find the bounding box of the non-white part of the image and crop it
             image_path = os.path.join(input_folder, filename)
-            bounding_box = find_non_white_bounding_boxL(image_path)
+            bounding_box = find_non_white_bounding_boxL(image_path, width, height)
             img = Image.open(image_path)
             cropped_img = img.crop(bounding_box)
 
             # Resize and save the resized image, ensuring it's also a PNG
-            resized_img = cropped_img.resize(target_size)
+            resized_img = cropped_img.resize((width, height))
             resized_png_output_path = os.path.join(output_folder, f"{filename.rsplit('.', 1)[0]}.png")
             resized_img.save(resized_png_output_path, "PNG")
             print(f"Resized {filename} and saved as {resized_png_output_path}\n")
     
 # given an image, find the bounding box of the non-transparent part of the image for a right page
-def find_non_white_bounding_boxR(image_path, min_width=10, min_height=10):
-    # change these values to match the desired width and height of the image for right pages
-    totalWidth = int(1936/5)
-    totalHeight = int(3351/5)
+def find_non_white_bounding_boxR(image_path, totalWidth, totalHeight, min_width=10, min_height=10):
 
     #First, get or create an image number
     #in this case, the image number is the 4th element of the split string
@@ -161,7 +155,7 @@ def find_non_white_bounding_boxR(image_path, min_width=10, min_height=10):
     return bounding_boxes[0]
 
 # Function to process images
-def process_imagesR(input_folder, output_folder, target_size):
+def process_imagesR(input_folder, output_folder, width, height):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
@@ -172,19 +166,19 @@ def process_imagesR(input_folder, output_folder, target_size):
             continue  # Skip macOS metadata files
 
         # Only process right page images
-        # The image is a right page if it ends with "_r.png" or "_r.png"
+        # The image is a right page if it ends with "_r.png"
         # change these string in the ends with function to match the naming convention of all your right page images
         if filename.lower().endswith(("_r.png", "_r.png")):
             print(filename)
 
             # Find the bounding box of the non-white part of the image and crop it
             image_path = os.path.join(input_folder, filename)
-            bounding_box = find_non_white_bounding_boxR(image_path)
+            bounding_box = find_non_white_bounding_boxR(image_pat, width, height)
             img = Image.open(image_path)
             cropped_img = img.crop(bounding_box)
             
             # Resize and save the resized image, ensuring it's also a PNG
-            resized_img = cropped_img.resize(target_size)
+            resized_img = cropped_img.resize((width, height))
             resized_png_output_path = os.path.join(output_folder, f"{filename.rsplit('.', 1)[0]}.png")
             resized_img.save(resized_png_output_path, "PNG")
             print(f"Resized {filename} and saved as {resized_png_output_path}\n")
@@ -194,18 +188,18 @@ def process_imagesR(input_folder, output_folder, target_size):
 # Set input and output folders as well as desired width and height for the images
 # put all of your photos to be processed in a folder labeled "input"
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-out = os.path.join(desktop, 'output')
-os.makedirs(out)
+input_folder = os.path.join(desktop, 'input')
+output_folder = os.path.join(desktop, 'output')
+os.makedirs(output_folder, exist_ok=True)
 
+#!!!
 desired_widthR = 1936
 desired_heightR = 3351
+
+#!!!
 desired_widthL = 2083
 desired_heightL = 3415
 
-# Set target size (width, height)
-target_sizeR = (desired_widthR, desired_heightR)
-target_sizeL = (desired_widthL, desired_heightL)
-
 # Process images
-process_imagesL(input_folder, output_folder, target_sizeL)
-process_imagesR(input_folder, output_folder, target_sizeR)
+process_imagesL(input_folder, output_folder, desired_widthL, desired_heightL)
+process_imagesR(input_folder, output_folder, desired_widthR, desired_heightR)
